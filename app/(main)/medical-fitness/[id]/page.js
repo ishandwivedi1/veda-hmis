1f8cb63e21c0cbc637f755a7798d16c8d5051d01
ishandwivedi1 <ishandwivedi1@gmail.com>
2026-07-24 07:45:40 +0000

    Consolidate Medical Fitness into single SPA page, matching Counselling architecture

--- app/(main)/medical-fitness/[id]/page.js
+++
@@ -1,7 +0,0 @@
-import MedicalFitnessWorkspace from './workspace';
-
-export default async function MedicalFitnessWorkspacePage({ params }) {
-  const { id } = await params;
-  return <MedicalFitnessWorkspace referralId={id} />;
-}
-
